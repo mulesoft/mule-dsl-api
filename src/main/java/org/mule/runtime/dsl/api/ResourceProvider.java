c5e37afa73ef9aab8dfe07e7cc065d196460775b
pablolagreca <Neruda10>
2018-04-19 16:42:06 -0300

    cosdf

---
+++ src/main/java/org/mule/runtime/dsl/api/ResourceProvider.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.dsl.api;
+
+import java.io.InputStream;
+
+import org.mule.api.annotation.NoImplement;
+
+/**
+ * Represents a generic resource provider, to be used instead of the artifact class loader.
+ *
+ * @since 1.2
+ */
+@NoImplement
+public interface ResourceProvider {
+
+  /**
+   * @param uri location of the resource. It may be a classpath location or an absolute path.
+   * @return the input stream of the resource or null if the resource was not found.
+   */
+  InputStream getResourceAsStream(String uri);
+}
---
+++ src/main/java/org/mule/runtime/dsl/internal/ClassLoaderResourceProvider.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.dsl.internal;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import org.mule.runtime.api.exception.MuleRuntimeException;
+import org.mule.runtime.dsl.api.ResourceProvider;
+
+/**
+ * Resource provider implementation that delegates to an actual class loader.
+ */
+public class ClassLoaderResourceProvider implements ResourceProvider
+{
+
+  private ClassLoader classLoader;
+
+  public ClassLoaderResourceProvider(ClassLoader classLoader) {
+    this.classLoader = classLoader;
+  }
+
+  @Override
+  public InputStream getResourceAsStream(String uri) {
+    InputStream resourceAsStream = classLoader.getResourceAsStream(uri);
+    if (resourceAsStream == null) {
+      File file = new File(uri);
+      if (file.exists()) {
+        try {
+          return new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+          throw new MuleRuntimeException(e);
+        }
+      }
+    }
+    return resourceAsStream;
+  }
+}
